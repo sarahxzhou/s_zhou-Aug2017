68536ba68007b84d5b701440c75c8a0bd8983642
sarahxzhou <18zhouh25@millshighsch.org>
2017-08-24 09:30:35 -0700

    Woohoo!

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,12 @@
+//Sarah Zhou
+//August 23, 2017
+//Project 2 -- Use print and println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi there!");
+		System.out.println("Hi there!");
+
+	}
+
+}

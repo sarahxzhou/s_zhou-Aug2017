08f64a4156e1edd052a32c15ce8514f44c0d25c6
sarahxzhou <18zhouh25@millshighsch.org>
2017-09-06 11:33:10 -0700

    Calculate Library

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,21 @@
+/*Sarah Zhou
+ * September 6, 2017
+ * This is the runner for the calculate library. 
+ * It is used to test math methods in Calculate.
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(9));
+		System.out.println(Calculate.cube(3));
+		System.out.println(Calculate.average(5, 4));
+		System.out.println(Calculate.average(6, 2, 4));
+		System.out.println(Calculate.toDegrees(90, 3.14159));
+		System.out.println(Calculate.toRadians(90, 3.14159));
+		System.out.println(Calculate.discriminant(3, 1, 2));
+		System.out.println(Calculate.toImproperFrac(2, 1, 3));
+		System.out.println(Calculate.toMixedNum(7, 2));
+		System.out.println(Calculate.foil(2, 3, 6, -7));
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,65 @@
+/*Sarah Zhou
+ * September 6, 2017
+ * Our self written math library
+ * It contains a series of methods to do basic math functions.
+ */
+public class Calculate {
+
+	// square the input, one integer to one integer
+	public static int square(int operand) {
+		return operand * operand;
+	}
+
+	// cube the input, one integer to one integer
+	public static int cube(int operand) {
+		return operand * operand * operand;
+	}
+
+	// determine the average of two doubles, two doubles to one double
+	public static double average(double operand1, double operand2) {
+		return (operand1 + operand2) / 2;
+	}
+
+	// determine the average of three doubles, three doubles to one double
+	public static double average(double operand1, double operand2, double operand3) {
+		return (operand1 + operand2 + operand3) / 3;
+	}
+
+	// convert an angle measure given in radians into degrees, two doubles to one
+	// double
+	public static double toDegrees(double operand, double pi) {
+		return ((operand / pi) * 180);
+	}
+
+	// convert an angle given in degrees into radians, two doubles to one double
+	public static double toRadians(double operand, double pi) {
+		return ((operand * pi) / 180);
+	}
+
+	// determine the discriminant of three doubles and returns a double
+	public static double discriminant(double a, double b, double c) {
+		return ((b * b) - (4 * a * c));
+	}
+
+	/*
+	 * converts mixed number into an improper fraction three integers into a string
+	 */
+	public static String toImproperFrac(int a, int b, int c) {
+		return ((a * c) + b) + "/" + c;
+	}
+
+	/*
+	 * converts improper fraction into a mixed number two integers into a string
+	 */
+	public static String toMixedNum(int a, int b) {
+		return (a / b) + " " + a % b + "/" + b;
+	}
+
+	/*
+	 * converts binomial multiplication of form (ax+b)(cx+d) into quadratic equation
+	 * of ax^2 +bx +c. accepts four integers and a String, returns a String
+	 */public static String foil(int a, int b, int c, int d) {
+		return a*c + "n^2" + "+" + (a*d + b*c) +"n" + b*d;
+	}
+
+}
